dae005394120aa1f8a61162ec5ef15a046b0b5e5
Vladimir Mishenskii <60189954+vovamishenskiy@users.noreply.github.com>
2023-05-09 21:49:44 +0400

    part3a exercises 3.1 - 3.4 done, todo: 3.5 - 3.8

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Welcome to phonebook app, backend part</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.send(response.json(persons))
+})
+
+app.get('/info', (request, response) => {
+    const date = new Date()
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.filter(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end
+    }
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
